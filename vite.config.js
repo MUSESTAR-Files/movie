e02ec990a04852fac53ec44015c2027e2cfb2599
MGYLRos28 <122721300+MGYLRos28@users.noreply.github.com>
2023-01-15 20:45:25 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import {svelte} from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
 	plugins: [svelte()],
 	server:{
-		host:'192.168.0.10',
+		host:'127.0.0.1',
 		port:2970
 	}
 });
