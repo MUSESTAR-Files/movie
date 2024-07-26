c7ffcb5f0ed9e0c9e872eb6317165914371ef5ea
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2024-07-26 22:57:30 +0800

    更新

--- src/lib/MobileUnder.svelte
+++ src/lib/MobileUnder.svelte
@@ -28,6 +28,7 @@
     <div class="mainBox">
         <a href="https://www.musestar.cc/" class="webMap"><span class="wiText">&copy; 2024 MUSESTAR</span></a>
         <a href="https://www.musestar.cc/" class="webMap"><span class="wiText">&copy; 2024 LycoriXradiaTa</span></a>
+        <a href="https://www.musestar.cc/" class="webMap"><span class="wiText">&copy; 2024 LingLoChan</span></a>
         <a href="http://ninnana.top/" class="webMap"><span class="wiText">&copy; 2024 Ninnana</span></a>
         <a href="https://icp.gov.moe/?keyword=20226956" class="webMap"><span class="wiText">萌ICP备20226956号</span></a>
         <i class="time">本站已运行 {sense}</i>
