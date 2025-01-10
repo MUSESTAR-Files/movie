10c63a57c3496e88edd5365ba8bbdc2040f9eef2
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2025-01-10 19:37:09 +0800

    Update BangumiList.js

--- src/js/BangumiList.js
+++ src/js/BangumiList.js
@@ -1,8 +1,8 @@
 //下面是滚动图列表
 export const TheImgList = ()=> {
 	return [
-		{src: "https://s1.imagehub.cc/images/2024/10/01/bbdf22953d7871c6e9e87908fa608651.jpg", href: '/film/Successor'},
-		{src: "https://s1.imagehub.cc/images/2024/08/01/66213bc0cf7da1118e8a07b0ea2c84f1.png", href: '/film/Deadpool3'}
+		{src: "https://s1.imagehub.cc/images/2024/12/14/591edfc09936e96d2d724a4f7adcf1f8.jpg", href: '/teleplay/SquidGame2/01'},
+		{src: "https://s1.imagehub.cc/images/2024/12/16/4830fb9ff71a01e44b066491e3468888.png", href: '/film/Venom3'}
 	];
 }
 //下面是电影列表
@@ -12,7 +12,7 @@ export const TheBangumiList = ()=>{
 			title:"搜索",
 			src:"https://video.musestar.cc/",
 			img:"https://api.anosu.top/api/?sort=all",
-			releaseDate:"2077,1,1",
+			releaseDate:"2077年1月1日",
 			classify:"自定义",
 			country:"搜索",
 			state:"第三方播放源在线播放"
@@ -21,7 +21,7 @@ export const TheBangumiList = ()=>{
 			title:"鱿鱼游戏 第二季",
 			src:"/teleplay/SquidGame2/01",
 			img:"https://s1.imagehub.cc/images/2024/12/16/4830fb9ff71a01e44b066491e3468888.png",
-			releaseDate:"2024,12,26",
+			releaseDate:"2024年12月26日",
 			classify:"动作",
 			country:"韩国",
 			state:"总集数: 7"
@@ -30,7 +30,7 @@ export const TheBangumiList = ()=>{
 			title:"毒液：最后一舞",
 			src:"/film/Venom3",
 			img:"https://s1.imagehub.cc/images/2024/12/14/591edfc09936e96d2d724a4f7adcf1f8.jpg",
-			releaseDate:"2024,10,23",
+			releaseDate:"2024年10月23日",
 			classify:"科幻",
 			country:"美国",
 			state:"影片时长: 1:48:55"
@@ -39,7 +39,7 @@ export const TheBangumiList = ()=>{
 			title:"出走的决心",
 			src:"/film/LikeARollingStone",
 			img:"https://s1.imagehub.cc/images/2024/11/24/466cbe99a3a590714b7557b29438c1be.jpg",
-			releaseDate:"2024,9,15",
+			releaseDate:"2024年9月15日",
 			classify:"剧情",
 			country:"中国",
 			state:"影片时长: 1:45:20"
@@ -48,7 +48,7 @@ export const TheBangumiList = ()=>{
 			title:"异形：夺命舰",
 			src:"/film/Alien",
 			img:"https://s1.imagehub.cc/images/2024/10/18/14263a888127c08369e650be307d5fd3.jpg",
-			releaseDate:"2024,8,16",
+			releaseDate:"2024年8月16日",
 			classify:"科幻",
 			country:"美国",
 			state:"影片时长: 1:59:45"
@@ -57,7 +57,7 @@ export const TheBangumiList = ()=>{
 			title:"死侍与金刚狼",
 			src:"/film/Deadpool3",
 			img:"https://s1.imagehub.cc/images/2024/08/01/66213bc0cf7da1118e8a07b0ea2c84f1.png",
-			releaseDate:"2024,7,26",
+			releaseDate:"2024年7月26日",
 			classify:"科幻",
 			country:"美国",
 			state:"影片时长: 2:08:38"
@@ -66,7 +66,7 @@ export const TheBangumiList = ()=>{
 			title:"抓娃娃",
 			src:"/film/Successor",
 			img:"https://s1.imagehub.cc/images/2024/10/01/bbdf22953d7871c6e9e87908fa608651.jpg",
-			releaseDate:"2024,7,16",
+			releaseDate:"2024年7月16日",
 			classify:"喜剧",
 			country:"中国",
 			state:"影片时长: 2:13:25"
@@ -75,7 +75,7 @@ export const TheBangumiList = ()=>{
 			title:"沙丘2",
 			src:"/film/Dune2",
 			img:"https://s1.imagehub.cc/images/2024/03/08/5c2d89b452e6219ac32d028e0e0fcaa5.jpeg",
-			releaseDate:"2024,3,8",
+			releaseDate:"2024年3月8日",
 			classify:"剧情",
 			country:"美国/加拿大",
 			state:"影片时长: 2:45:47"
@@ -84,7 +84,7 @@ export const TheBangumiList = ()=>{
 			title:"周处除三害",
 			src:"/film/zccsh",
 			img:"https://s1.imagehub.cc/images/2024/03/08/77c45a12233fb2ee4a9e128dfe733fe7.jpeg",
-			releaseDate:"2024,3,1",
+			releaseDate:"2024年3月1日",
 			classify:"犯罪",
 			country:"中国",
 			state:"影片时长: 2:14:00"
@@ -93,7 +93,7 @@ export const TheBangumiList = ()=>{
 			title:"第二十条",
 			src:"/film/Article20",
 			img:"https://s1.imagehub.cc/images/2024/02/15/fadac74cbd0dff1825d7e446d46e1fe9.jpeg",
-			releaseDate:"2024,2,10",
+			releaseDate:"2024年2月10日",
 			classify:"剧情",
 			country:"中国",
 			state:"影片时长: 2:23:23"
@@ -102,7 +102,7 @@ export const TheBangumiList = ()=>{
 			title:"飞驰人生2",
 			src:"/film/Pegasus2",
 			img:"https://s1.imagehub.cc/images/2024/02/15/00afacb70f62c5edbcefb9da52f6d553.jpeg",
-			releaseDate:"2024,2,10",
+			releaseDate:"2024年2月10日",
 			classify:"剧情",
 			country:"中国",
 			state:"影片时长: 1:56:32"
@@ -111,7 +111,7 @@ export const TheBangumiList = ()=>{
 			title:"我们一起摇太阳",
 			src:"/film/VivaLaVida",
 			img:"https://s1.imagehub.cc/images/2024/05/26/80ab593da3c62dd3dff35791e8865fd7.webp",
-			releaseDate:"2024,2,10",
+			releaseDate:"2024年2月10日",
 			classify:"剧情",
 			country:"中国",
 			state:"影片时长: 2:09:53"
@@ -120,7 +120,7 @@ export const TheBangumiList = ()=>{
 			title:"年会不能停！",
 			src:"/film/JohnnyKeepWalking",
 			img:"https://s1.imagehub.cc/images/2024/02/14/a569df061c2f79f77988379669460c21.jpeg",
-			releaseDate:"2023,12,29",
+			releaseDate:"2023年12月29日",
 			classify:"喜剧",
 			country:"中国",
 			state:"影片时长: 1:57:04"
@@ -129,7 +129,7 @@ export const TheBangumiList = ()=>{
 			title:"三大队",
 			src:"/film/EndlessJourney",
 			img:"https://s1.imagehub.cc/images/2024/02/02/c7f84824f4f786661086ac7390159a37.jpeg",
-			releaseDate:"2023,12,15",
+			releaseDate:"2023年12月15日",
 			classify:"犯罪",
 			country:"中国",
 			state:"影片时长: 2:12:59"
@@ -138,7 +138,7 @@ export const TheBangumiList = ()=>{
 			title:"涉过愤怒的海",
 			src:"/film/AcrosstheFuriousSea",
 			img:"https://s1.imagehub.cc/images/2024/01/13/cf3a7494cdcfb3eee68c3f0b3478b005.jpeg",
-			releaseDate:"2023,11,25",
+			releaseDate:"2023年11月25日",
 			classify:"悬疑",
 			country:"中国",
 			state:"影片时长: 2:23:55"
@@ -147,7 +147,7 @@ export const TheBangumiList = ()=>{
 			title:"洛基 第二季",
 			src:"/teleplay/Loki2",
 			img:"https://s1.imagehub.cc/images/2023/11/03/09757e6198a5efc12bb76a8cfa68b26b.png",
-			releaseDate:"2023,10,5",
+			releaseDate:"2023年10月5日",
 			classify:"科幻",
 			country:"美国",
 			state:"总集数: 6"
@@ -156,7 +156,7 @@ export const TheBangumiList = ()=>{
 			title:"坚如磐石",
 			src:"/film/UndertheLight",
 			img:"https://s1.imagehub.cc/images/2023/12/15/1eb3605cf794cfbcca3e153ba329089c.jpeg",
-			releaseDate:"2023,9,28",
+			releaseDate:"2023年9月28日",
 			classify:"犯罪",
 			country:"中国",
 			state:"影片时长: 2:07:32"
@@ -165,7 +165,7 @@ export const TheBangumiList = ()=>{
 			title:"奥本海默",
 			src:"/film/Oppenheimer",
 			img:"https://s1.imagehub.cc/images/2023/08/30/Oppenheimer.webp",
-			releaseDate:"2023,8,30",
+			releaseDate:"2023年8月30日",
 			classify:"剧情",
 			country:"美国",
 			state:"影片时长: 3:00:22"
@@ -174,7 +174,7 @@ export const TheBangumiList = ()=>{
 			title:"国民死刑投票",
 			src:"/teleplay/TheKillingVote",
 			img:"https://s1.imagehub.cc/images/2023/08/25/TheKillingVote.webp",
-			releaseDate:"2023,8,10",
+			releaseDate:"2023年8月10日",
 			classify:"剧情",
 			country:"韩国",
 			state:"总集数: 12"
@@ -183,7 +183,7 @@ export const TheBangumiList = ()=>{
 			title:"孤注一掷",
 			src:"/film/NoMoreBets",
 			img:"https://s1.imagehub.cc/images/2023/08/17/NoMoreBets.webp",
-			releaseDate:"2023,8,8",
+			releaseDate:"2023年8月8日",
 			classify:"犯罪",
 			country:"中国",
 			state:"影片时长: 2:09:55"
@@ -192,7 +192,7 @@ export const TheBangumiList = ()=>{
 			title:"消失的她",
 			src:"/film/LostintheStars",
 			img:"https://s1.imagehub.cc/images/2023/08/17/LostintheStars.webp",
-			releaseDate:"2023,6,22",
+			releaseDate:"2023年6月22日",
 			classify:"悬疑",
 			country:"中国",
 			state:"影片时长: 2:01:43"
@@ -201,7 +201,7 @@ export const TheBangumiList = ()=>{
 			title:"闪电侠",
 			src:"/film/Flash",
 			img:"https://s1.imagehub.cc/images/2023/07/18/Flash.webp",
-			releaseDate:"2023,6,16",
+			releaseDate:"2023年6月16日",
 			classify:"科幻",
 			country:"美国",
 			state:"影片时长: 2:07:19"
@@ -210,7 +210,7 @@ export const TheBangumiList = ()=>{
 			title:"速度与激情10",
 			src:"/film/Fast10",
 			img:"https://s1.imagehub.cc/images/2023/06/12/Fast10.webp",
-			releaseDate:"2023,5,17",
+			releaseDate:"2023年5月17日",
 			classify:"动作",
 			country:"美国",
 			state:"影片时长: 2:20:50"
@@ -219,7 +219,7 @@ export const TheBangumiList = ()=>{
 			title:"银河护卫队3",
 			src:"/film/GuardiansoftheGalaxy3",
 			img:"https://s1.imagehub.cc/images/2023/05/14/GuardiansoftheGalaxy3.webp",
-			releaseDate:"2023,5,5",
+			releaseDate:"2023年5月5日",
 			classify:"科幻",
 			country:"美国",
 			state:"影片时长: 2:30:02"
@@ -228,7 +228,7 @@ export const TheBangumiList = ()=>{
 			title:"俄罗斯方块",
 			src:"/film/Tetris",
 			img:"https://s1.imagehub.cc/images/2023/04/15/6b8deba55eca287f19146c2db71f43d5.webp",
-			releaseDate:"2023,3,31",
+			releaseDate:"2023年3月31日",
 			classify:"传记",
 			country:"美国",
 			state:"影片时长: 1:57:44"
@@ -237,7 +237,7 @@ export const TheBangumiList = ()=>{
 			title:"保你平安",
 			src:"/film/PostTruth",
 			img:"https://s1.imagehub.cc/images/2023/05/14/PostTruth.webp",
-			releaseDate:"2023,3,10",
+			releaseDate:"2023年3月10日",
 			classify:"剧情",
 			country:"中国",
 			state:"影片时长: 1:51:24"
@@ -246,7 +246,7 @@ export const TheBangumiList = ()=>{
 			title:"流浪地球2",
 			src:"/film/TheWanderingEarth2",
 			img:"https://s1.imagehub.cc/images/2023/01/02/49348cf0d5976eb8567f845bb616ae38.webp",
-			releaseDate:"2023,1,22",
+			releaseDate:"2023年1月22日",
 			classify:"科幻",
 			country:"中国",
 			state:"影片时长: 2:53:22"
@@ -255,7 +255,7 @@ export const TheBangumiList = ()=>{
 			title:"阿凡达2：水之道",
 			src:"/film/Avatar2",
 			img:"https://s1.imagehub.cc/images/2022/12/16/852ece4d2e2d5d3347f0027b2528f0e6.webp",
-			releaseDate:"2022,12,16",
+			releaseDate:"2022年12月16日",
 			classify:"科幻",
 			country:"美国",
 			state:"影片时长: 3:04:31"
@@ -269,7 +269,7 @@ export const TheBangumiMoveList = ()=>{
 			title:"搜索剧集",
 			src:"https://video.musestar.cc/",
 			img:"https://api.anosu.top/api/?sort=all",
-			releaseDate:"2024,1,1",
+			releaseDate:"2024年1月1日",
 			classify:"自定义",
 			country:"搜索",
 			state:"第三方播放源在线播放"
